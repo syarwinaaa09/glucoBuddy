653b831e11a71c4ad53dec9a9b370723cdd31492
Green-Tea-123 <junboonsoo@gmail.com>
2023-07-07 17:16:57 +0800

    login screen skeleton (mmm bones)

---
+++ components/login.tsx
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function login() {
+    return (
+      <View>
+        <Text style={styles.title}>Login</Text>
+        <View>
+            <Text style={styles.text}>Email:</Text>
+            <TextInput style={styles.input}></TextInput>
+            <Text style={styles.text}>Password:</Text>
+            <TextInput style={styles.input}></TextInput>
+            <Pressable style={styles.button}>
+              <Text style={styles.buttontext}>
+                Login
+              </Text>
+            </Pressable>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+const styles = StyleSheet.create({
+  title:{
+    fontSize:50,
+    textAlign:'center'
+  },
+  text:{
+    marginHorizontal:5
+  },
+  input:{
+    backgroundColor:'rgb(200,200,200)',
+    margin:5
+  },
+  button:{
+    marginHorizontal:5,
+    marginVertical:10,
+    textAlign:'center',
+    borderRadius:4,
+    backgroundColor:'black',
+    padding:5
+  },
+  buttontext:{
+    color:'white'
+  }
+})
