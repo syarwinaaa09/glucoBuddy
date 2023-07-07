3c3c9ef5eb46c282a1227f5367f3080bcfdb4c9b
Syarwina <syarwinaaa09@gmail.com>
2023-07-07 20:55:58 +0800

    Progress Ring UI

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Value from './src/components/Value';
+import RingProgress from './src/components/RingProgress';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
+    <View style={styles.container}> 
+      <RingProgress progress={0.56} />
 
+      <StatusBar style="auto" />
       <View style={styles.values}>
         <Value label="Steps" value="1219" />
-        <Value label="Distance" value="0,75 km" />
+        <Value label="Distance" value="0.75 km" />
         <Value label="Flights Climbed" value="12" />
       </View>
 
@@ -29,5 +31,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: 25,
     flexWrap: 'wrap',
+    marginTop: 100,
   },
 });
