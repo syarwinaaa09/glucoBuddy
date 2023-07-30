541e53c6e2ca9201c6a824c359446540b10ee649
Syarwina <syarwinaaa09@gmail.com>
2023-07-30 12:46:03 +0800

    remove android/ios

--- App.tsx
+++ App.tsx
@@ -5,13 +5,12 @@ import RingProgress from './src/components/RingProgress';
 
 export default function App() {
   return (
-    <View style={styles.container}> 
-      <RingProgress progress={0.5} />
+    <View style={styles.container}>
+      <RingProgress radius={150} strokeWidth={50} progress={0.5} />
 
-      <StatusBar style="auto" />
       <View style={styles.values}>
         <Value label="Steps" value="1219" />
-        <Value label="Distance" value="0.75 km" />
+        <Value label="Distance" value="0,75 km" />
         <Value label="Flights Climbed" value="12" />
       </View>
 
