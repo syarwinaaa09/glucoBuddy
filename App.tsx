815986b45022e5d3fe72cfa34960e0e3d8bcdcff
Green-Tea-123 <junboonsoo@gmail.com>
2023-07-31 11:27:34 +0800

    why doesn't it work?????

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
+import * as React from 'react';
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Glucometer from "./screens/glucometer";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Glucometer />
-
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Glucometer" component={Glucometer} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
